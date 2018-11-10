d88b745fb613a7e644ee9d7e33773f6be9a6a4d2
Raquel McCallum <raquelmccallum@gmail.com>
2018-11-10 19:38:08 +0000

    fixed the slideshow, css, and added new font

--- javascript.js
+++ javascript.js
@@ -1,16 +1,17 @@
 // var (variable) can be updated and modified
 // the for loop makes all of pur images invisible, the style.display="block" makes a single image visible
-var slideIndex=0;
+var slideIndex = 0;
 showSlides();
 function showSlides(){
-    var slides=document.getElementsByClassName("myImages");
+    // console.log('Hello',slideIndex)
+    var slides = document.getElementsByClassName("myImages");
     // retreives anything with the name "myImages"
     for(var i=0; i<slides.length;i++){
         slides[i].style.display="none";
         // slides= array[img1,img2,img3], style.display=>accesses and changing the images attributes
     }
     slideIndex++;
-    if(slideIndex>=slides.length){
+    if(slideIndex >= slides.length){
         slideIndex=0;
     }
     slides[slideIndex].style.display="block";
